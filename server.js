4f816c7724d36e5aa385c5e187b69958c365b403
Timothy Tomas <timothyjtee@gmail.com>
2022-06-16 18:02:50 -0700

    implement home get request

--- server.js
+++ server.js
@@ -24,3 +24,15 @@ const festivals = {
             'image':'https://images.unsplash.com/photo-1505224628533-c4fc42c389e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2834&q=80',
         }
     }
+
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html');
+    })
+    
+    app.get('/api/:festivalName', (req, res) => {
+      const festivalName = req.params.festivalName.toLowerCase();
+    
+      if(Object.hasOwn(festivals, festivalName)) {
+        res.json(festivals[0][festivalName]);
+      }
+    });
