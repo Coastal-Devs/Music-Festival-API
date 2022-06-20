aa2fef22cf77ef1b9bb86d3648b1910ce3227546
Dana Lee <theoriginaldanalee@gmail.com>
2022-06-20 19:25:25 -0400

    objects with image links for testing

--- festivals.js
+++ festivals.js
@@ -7,8 +7,8 @@ const festivals = [
             description: 'The Coachella Valley Music and Arts Festival is an annual music and arts festival held at the Empire Polo Club in Indio, California, in the Coachella Valley in the Colorado Desert. It was co-founded by Paul Tollett and Rick Van Santen in 1999, and is organized by Goldenvoice, a subsidiary of AEG Presents.',
             attendance: '125,000',
             website: 'coachella.com',
-            picture:'',
-        }
+            picture: 'https://www.coachella.com/',
+        },
         'Governor\'s Ball': {
             location: 'Citifield, New York City',
             date: 'TBD',
@@ -17,7 +17,16 @@ const festivals = [
             attendance: '150,000',
             website: 'https://www.governorsballmusicfestival.com/',
             picture:'',
-        }
+        },
+        'Governor\'s Ball': {
+            location: 'Citifield, New York City',
+            date: 'TBD',
+            genre: 'rock, pop, indie, hip hop and electronic dance music',
+            description: 'The Gov Ball experience encompasses all of these and is unlike any other festival out there. Built by New Yorkers, for New Yorkers, the festival is always evolving, always entertaining, and always striving to exceed your expectations.',
+            attendance: '150,000',
+            website: 'https://www.governorsballmusicfestival.com/',
+            picture:'',
+        },
 
 
     }
