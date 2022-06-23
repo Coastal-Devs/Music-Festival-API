2004df89eabb59c4f21522d13ebd5039a8552d15
Dana Lee <theoriginaldanalee@gmail.com>
2022-06-22 21:59:06 -0400

    changes to API

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 const festivals = {
-    
-    'coachella': {
+    //, pop, indie, hip hop and electronic dance music'
+    'rock' :{
+        'name': 'Coachella',
         'location': 'Los Angeles, California',
         'date': 'April 14-16, 21-23 2023',
-        'genre': 'rock, pop, indie, hip hop and electronic dance music',
         'description': 'The Coachella Valley Music and Arts Festival is an annual music and arts festival held at the Empire Polo Club in Indio, California, in the Coachella Valley in the Colorado Desert. It was co-founded by Paul Tollett and Rick Van Santen in 1999, and is organized by Goldenvoice, a subsidiary of AEG Presents.',
         'attendance': '125,000',
         'website': 'coachella.com',
