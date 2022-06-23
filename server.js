81bbc16d3777402b6f8827fe7378309b1140b5ac
Dana Lee <theoriginaldanalee@gmail.com>
2022-06-23 16:25:26 -0400

    issue1

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 const festivals = {
-    'rock pop indie hip hop and electronic dance music' :{
+    'rock pop indie hip hop electronic dance':{
         'name': 'Coachella',
         'location': 'Los Angeles, California',
         'date': 'April 14-16, 21-23 2023',
+        'genre': 'rock pop indie hip hop electronic dance',
         'description': 'The Coachella Valley Music and Arts Festival is an annual music and arts festival held at the Empire Polo Club in Indio, California, in the Coachella Valley in the Colorado Desert. It was co-founded by Paul Tollett and Rick Van Santen in 1999, and is organized by Goldenvoice, a subsidiary of AEG Presents.',
         'attendance': '125,000',
         'website': 'coachella.com',
@@ -17,12 +18,13 @@ const festivals = {
         'name': 'Governor\'s Ball',
         'location': 'Citifield, New York City',
         'date': 'TBD',
+        'genre': 'rock, pop, indie, hip hop and electronic dance music',
         'description': 'The Gov Ball experience encompasses all of these and is unlike any other festival out there. Built by New Yorkers, for New Yorkers, the festival is always evolving, always entertaining, and always striving to exceed your expectations.',
         'attendance': '150,000',
         'website': 'https://www.governorsballmusicfestival.com/',
         'picture':'https://www.governorsballmusicfestival.com/wp-content/uploads/2021/09/POST2021_0926_220307_7313_ALIVECOVERAGE_CTL_5vqeu8wjc07hb46ilr9m-scaled.jpeg',
     },
-    'Lollapalooza': {
+    'alternative-rock, punk-rock': {
         'name': 'Lollapalooza',
         'location': 'Grant Park, Chicago, Illinois',
         'date': 'July 28-31, 2022',
@@ -33,7 +35,8 @@ const festivals = {
         'picture':'https://assets-global.website-files.com/5e927ba01e4ad56ae5465eb8/62a120fd4aaf2d18b41a2569_L22-Daily-0608-1080x1350.png',
     },
 
-    'Ultra Music Festival': {
+    'electronic': {
+        'name': 'Ultra Music Festival',
         'location': 'Miami, Florida',
         'date': 'March 24-26, 2023',
         'genre': 'electronic',
